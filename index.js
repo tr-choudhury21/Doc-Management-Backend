a10f3af64bbb4e4dadb64dc4eaf75ccb69fa3320
Titu Ray choudhury <tituraychoudhury@gmail.com>
2025-01-13 19:16:22 +0530

    define routes in the main server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const dbConnection = require('./database/dbConnection');
+const uploadRoutes = require('./routes/uploadRoutes');
 
 dotenv.config();
 
@@ -10,8 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('uploads'));
 
+//routes
+app.use('/api/upload', uploadRoutes);
 
+
+
+//database connection
 dbConnection();
+
+
 
 const PORT = process.env.PORT || 5000;
 
